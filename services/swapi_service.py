fc4a3ac9e80bd31dcd19fa705de3e4f2c1c1afe8
Chris Fahim <6053415+cfahim@users.noreply.github.com>
2025-03-18 02:46:54 -0600

    added schemas for all endpoints, added openapi support for aws gateway

--- services/swapi_service.py
+++ services/swapi_service.py
@@ -1,7 +1,8 @@
 import repositories.swapi_repository as SWAPIRepository
+from schemas.swapi_schema import FilmResponse
 
 
 class SWAPIService:
     @staticmethod
-    async def get_all_films():
+    async def get_all_films() -> FilmResponse | None:
         return await SWAPIRepository.SWAPIRepository.get_all_films()
