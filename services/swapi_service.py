c397d67f5eeaf5471bbb5d0412b1888ac30c9b34
cfahim <cfahim@users.noreply.github.com>
2025-04-10 00:34:00 -0600

    added swapi to DI (#8)
    * added dependency injection and added alembic orm
    
    * added new dependencies for alembic,postgres,di into requirements.txt
    
    * added swapi to DI process
    
    ---------
    
    Co-authored-by: Chris Fahim <6053415+cfahim@users.noreply.github.com>

--- services/swapi_service.py
+++ services/swapi_service.py
@@ -3,6 +3,9 @@ from schemas.swapi_schema import FilmResponse
 
 
 class SWAPIService:
-    @staticmethod
-    async def get_all_films() -> FilmResponse | None:
-        return await SWAPIRepository.SWAPIRepository.get_all_films()
+    def __init__(self, swapi_repository: SWAPIRepository.SWAPIRepository): 
+        self.swapi_repository = swapi_repository
+
+    
+    async def get_all_films(self) -> FilmResponse | None:
+        return await self.swapi_repository.get_all_films()
